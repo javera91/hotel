426c323672e30d7c64b18bc6c5ee6ee644248d5c
javera91 <146044960+javera91@users.noreply.github.com>
2024-11-13 18:20:43 -0500

    changed1

--- src/main/kotlin/com/example/hotel/controller/GuestController.kt
+++ src/main/kotlin/com/example/hotel/controller/GuestController.kt
@@ -2,9 +2,12 @@ package com.example.hotel.controller
 
 import com.example.hotel.dto.GuestDto
 import com.example.hotel.entity.Guest
+import com.example.hotel.response.SuccessResponse
 import com.example.hotel.service.GuestService
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -14,11 +17,27 @@ class GuestController {
     lateinit var guestService: GuestService
 
     @GetMapping
-    fun getGuests() = guestService.getGuests()
+    fun getGuests() : ResponseEntity<*> {
+        val response= guestService.getGuests()
+        return ResponseEntity(
+            SuccessResponse().apply {
+                status = "success"
+                data = response
+            },HttpStatus.OK)
+    }
 
     @PostMapping
-    fun save (@RequestBody @Valid guestDto: GuestDto): Guest {
-        return guestService.save(guestDto)
+    fun save (@RequestBody @Valid guestDto: GuestDto): ResponseEntity<*> {
+        val response= guestService.save(guestDto)
+        return ResponseEntity (
+            SuccessResponse().apply {
+                status = "success"
+                data = response
+            },
+            HttpStatus.CREATED)
 
     }
+
+
+
 }
--- src/main/kotlin/com/example/hotel/response/SuccessResponse.kt
+++ src/main/kotlin/com/example/hotel/response/SuccessResponse.kt
@@ -1,4 +1,6 @@
 package com.example.hotel.response
 
 class SuccessResponse {
+    var status : String? = null
+    var data : Any? = null
 }
